6d0682aee763dcf90c346dea95a16c1a05a7f3c1
12400506 <12400506@student.pxl.be>
2024-10-21 03:50:19 +0200

    afgewerkt

---
+++ faculteit/faculteit/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace faculteit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            numberTextBox.Clear();
+            resultTextBox.Clear();
+            numberTextBox.Focus();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //input
+            double number = double.Parse(numberTextBox.Text);
+            double result = 1;
+            //berekening
+            do
+            {
+                result *= number;  // vermenigvuldig het getal met nummer
+                number--;          // doe nummer -1
+            }
+            while (number > 0);
+            //output
+            resultTextBox.Text = result.ToString();
+        }
+    }
+}
---
+++ snelste atleet/snelste atleet/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snelste_atleet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //lijst voor alle athelten
+        private List<name> atleet = new List<atleet>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            // declareer mijn variable
+            string name = nameTextBox.Text;
+            int time = int.Parse(timeTextBox.Text);
+            int fastest;
+            // testen of atheleet een naam heeft
+            if (int.TryParse(timeTextBox.Text, out time) && !string.IsNullOrWhiteSpace(name))
+            {
+                
+            }
+
+
+            int hour = time / 60;
+            int minute = (time % 3600) / 60;
+            int second = time % 60;
+
+
+            
+            resultTextBox.Text = $"De tijd van de atleet is {hour} uren en {minute}minuten en {second}seconden";
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+            nameTextBox.Clear();
+        }
+    }
+}
